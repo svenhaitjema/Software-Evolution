adab2c3726bc5d967af5a212ecaf07d31cd6b47f
Svenhaitjema <svenerik.haitjema@gmail.com>
2013-11-18 16:51:02 +0100

    Example project to test metric suite

---
+++ test_project/hello/src/hello/Main.java
@@ -0,0 +1,32 @@
+package hello;
+
+/* multi line */
+/* bla
+ * sa
+ */
+
+//hoi
+
+public class Main {
+	/* in class test 
+	 * werkt niet
+	 */
+	public static void main()
+	{
+		 System.out.println("Hello world"); // bla
+	}
+	
+	public void testing()
+	{
+	 System.out.println("bla");
+	 
+	 /* dit is een comment test */
+	 
+	 // om te kijken
+	 // of het totale
+	 // project 
+	 
+	 // wel //werkt
+	}
+
+}
---
+++ test_project/hello/src/hello/Second.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class Second {
+
+	private int a;
+
+	public Second(int a){
+		this.a = a;
+		
+	}
+	
+	public void bla() // Test
+	{
+	
+	     int a = 0;
+	     if(a == 0)
+	     {
+	    	 System.out.println("Solar system");
+	     }
+	     else
+	     {
+	    	 System.out.println("World domination");
+	     }
+	}	
